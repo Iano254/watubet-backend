91c3cd3aa3da04a4aa7f2976280478a0cde00e13
Ian Oumo <iano254@Ians-MacBook-Pro.local>
2025-12-15 11:47:01 +0300

    Import from ts

--- betManager.ts
+++ betManager.ts
@@ -6,21 +6,23 @@ const prisma = new PrismaClient();
 export interface Bet {
   id: string;
   userId: string;
+  walletId: string;
   amount: number;
   gameId: string;
   cashoutAt: number | null;
 }
 
-export async function getCurrentBets(gameId: string): Promise<Bet[]> {
-  return prisma.bet.findMany({
-    where: { gameId: gameId },
-    select: { id: true, userId: true, amount: true, gameId: true, cashoutAt: true }
-  });
-}
-
 export async function addBet(bet: Omit<Bet, 'id'>): Promise<Bet> {
   return prisma.bet.create({ data: bet });
 }
+
+export async function getCurrentBets(gameId: string): Promise<Bet[]> {
+  return prisma.bet.findMany({
+    where: { gameId: gameId },
+    select: { id: true, userId: true, amount: true, gameId: true, cashoutAt: true, walletId: true }
+  });
+}
+
 
 export async function updateBet(id: string, cashoutAt: number): Promise<Bet> {
   return prisma.bet.update({
